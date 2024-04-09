c791d11e3a011a5c34b1b5e6d0aeef73a4bab554
jho-my <ojedasnachezjhonatan@gmail.com>
2024-04-09 16:25:50 -0500

    Clase 1 tipos de datos basicos  en C++

---
+++ Clase01-Tipos-de-datos/Tiposdedatos.cpp
@@ -0,0 +1,18 @@
+#include <iostream>;
+using namespace std;
+
+int main(){
+    //dato entero
+    int entero = 8;
+    //dato float o decimal
+    float decimal = 8.34;
+    //dato double tambien decimal 
+    double decimal2 = 3.1416154;
+    //dato char
+    char letra = 'J';
+    cout<<entero;
+    cout<<decimal;
+    cout<<decimal2;
+    cout<<letra;
+    return 0;
+}
