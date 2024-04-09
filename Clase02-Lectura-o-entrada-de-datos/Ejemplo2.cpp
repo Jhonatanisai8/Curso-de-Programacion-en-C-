0ed87d780192141c1c5dca1c33e9dc398e5c2c2c
jho-my <ojedasnachezjhonatan@gmail.com>
2024-04-09 16:58:11 -0500

    Lectura y entrada de datos

---
+++ Clase02-Lectura-o-entrada-de-datos/Ejemplo2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string nombre;
+    int edad;
+    cout << "Ingresa tu nombre por favor: ";
+    cin >> nombre;
+    cout << "Ingresa tu edad:";
+    cin >> edad;
+    cout << "Tu nombre es: " << nombre << ".Tienes " << edad << " años.\n";
+}
---
+++ Clase02-Lectura-o-entrada-de-datos/entrada.cpp
@@ -0,0 +1,13 @@
+//lectura de datos
+#include <iostream>
+using namespace std;
+
+int main(){
+    int numero;
+    cout <<"Digite un numero: ";
+    //leemos el numero
+    cin>>numero;
+
+    cout<<"\nEl numero que digito es:"<<numero<<"\n";
+    return 0;
+}
